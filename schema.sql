6458b1ec631fb54c9cbcd98cb2c31fa334993b23
Andrew Connally <ajconnally93@gmail.com>
2022-11-22 21:18:15 -0600

    added schema.sql and seeds.sql with SOURCEing instructions

---
+++ schema.sql
@@ -0,0 +1,29 @@
+-- SOURCE SCHEMA.SQL FIRST - BEFORE SEEDS.SQL
+
+DROP DATABASE IF EXISTS employeesDB;
+CREATE DATABASE employeesDB;
+USE employeesDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NULL,
+  PRIMARY KEY (id)
+);
+
+-- could also use TINYTEXT with VARCHAR NULL, sets 255 character limit
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NULL,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NULL,
+  last_name VARCHAR(45) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
